8d5778490f9b66caf0036f8164c3d2bd92011fde
DTG-J <grozdanovdt@abv.bg>
2024-05-26 14:23:03 +0300

    Update Comment.java

--- Pathfinder/src/main/java/bg/softuni/pathfinder/model/Comment.java
+++ Pathfinder/src/main/java/bg/softuni/pathfinder/model/Comment.java
@@ -29,6 +29,55 @@ public class Comment {
     @ManyToOne(optional = false, fetch = FetchType.LAZY)
     private Route route;
 
-   private Comment () {}
+    public Comment() {
 
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public boolean isApproved() {
+        return approved;
+    }
+
+    public void setApproved(boolean approved) {
+        this.approved = approved;
+    }
+
+    public Instant getCreated() {
+        return created;
+    }
+
+    public void setCreated(Instant created) {
+        this.created = created;
+    }
+
+    public String getTextContent() {
+        return textContent;
+    }
+
+    public void setTextContent(String textContent) {
+        this.textContent = textContent;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public Route getRoute() {
+        return route;
+    }
+
+    public void setRoute(Route route) {
+        this.route = route;
+    }
 }
