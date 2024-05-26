e38c75cc30bdad5a80eff77a6f201f81c9a67dbd
DTG-J <grozdanovdt@abv.bg>
2024-05-26 10:47:08 +0300

    added user entity

---
+++ Pathfinder/src/main/java/bg/softuni/pathfinder/model/User.java
@@ -0,0 +1,38 @@
+package bg.softuni.pathfinder.model;
+
+import jakarta.persistence.*;
+import org.hibernate.sql.results.internal.StandardEntityGraphTraversalStateImpl;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table (name = "users")
+public class User {
+     @Id
+     @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private long id;
+    private Integer age;
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @Column(name = "full_name", nullable = false)
+    private String fullName;
+    private String email;
+    @ManyToMany
+    private Set<Role> roles;
+    @Enumerated (EnumType.STRING)
+    private UserLevel level;
+    public User () {
+        this.roles = new HashSet<> ();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
