9a53f703d0fbad86d269b685a0a385cb298fd2d2
DTG-J <grozdanovdt@abv.bg>
2024-06-02 18:37:16 +0300

    UserRegisterController added

---
+++ Pathfinder/src/main/java/bg/softuni/pathfinder/web/UserController.java
@@ -0,0 +1,21 @@
+package bg.softuni.pathfinder.web;
+
+import bg.softuni.pathfinder.web.dto.UserRegisterDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+    @GetMapping("users/register")
+    public String viewRegister(){
+        return "register";
+    }
+
+    @PostMapping("user/register")
+    public String doRegister(UserRegisterDTO data){
+        return "index";
+    }
+
+
+}
