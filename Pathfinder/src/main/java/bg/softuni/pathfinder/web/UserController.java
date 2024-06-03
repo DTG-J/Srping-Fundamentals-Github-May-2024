6ddcafb81fa4cbb006933496aa6cdcf148f6f875
DTG-J <grozdanovdt@abv.bg>
2024-06-03 18:42:17 +0300

    Update UserController.java

--- Pathfinder/src/main/java/bg/softuni/pathfinder/web/UserController.java
+++ Pathfinder/src/main/java/bg/softuni/pathfinder/web/UserController.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.net.http.HttpClient;
 
 @Controller
 public class UserController {
@@ -15,8 +18,12 @@ public class UserController {
     }
 
     @PostMapping("user/register")
-    public String doRegister(@Valid UserRegisterDTO data, BindingResult bindingResult){
+    public String doRegister(@Valid UserRegisterDTO data,
+                             BindingResult bindingResult,
+                             RedirectAttributes redirectAttributes
+    ){
         if (bindingResult.hasErrors ()){
+            redirectAttributes.addAttribute ("registerData", data);
             //handle errors
             return "register";
         }
